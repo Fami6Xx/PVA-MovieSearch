89a5063d09eb5329896c314f8dfef5eea36f4bc4
Fami6Xx <libojer@gmail.com>
2025-04-01 09:27:24 +0200

    feat: update getServerSideProps to use authOptions from lib/auth

--- lib/getServerSideProps.ts
+++ lib/getServerSideProps.ts
@@ -1,5 +1,5 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { getServerSession } from "next-auth/next"
+import {authOptions} from "@/lib/auth";
 
 export async function getServerSideProps(context: any) {
   const session = await getServerSession(context.req, context.res, authOptions)
