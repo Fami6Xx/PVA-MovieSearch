79a53224a13db7b8b2fcab1a54b6bbfd5cf61c22
Fami6Xx <libojer@gmail.com>
2025-04-01 09:09:34 +0200

    feat: add layout component for session management and redirect to home

---
+++ app/(loggedout)/layout.js
@@ -0,0 +1,17 @@
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "@/lib/auth";
+import {redirect} from "next/navigation";
+
+const Layout = async ({children}) => {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    return redirect("/");
+  }
+  return (
+    <>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
